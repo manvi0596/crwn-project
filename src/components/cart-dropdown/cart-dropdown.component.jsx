697334f704d6ebdd891d07daacd856b3bba9fe91
manvi0596 <manvichandaliya7@gmail.com>
2021-10-03 15:07:01 +0530

    updating our application with reselect,replaced cart selectors with memoized selectors

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,6 +3,7 @@ import CustomButton from "../custom-button/custom-button.component";
 import { connect } from "react-redux";
 import "./cart-dropdown.styles.scss";
 import CartItem from "../cart-item/cart-item.component";
+import { selectCartItems } from "../../redux/cart/cart.selectors";
 
 const CartDropdown = ({ cartItems }) => {
   return (
@@ -17,9 +18,9 @@ const CartDropdown = ({ cartItems }) => {
   );
 };
 
-const mapStateToProps = ({ cart: { cartItems } }) => {
+const mapStateToProps = (state) => {
   return {
-    cartItems,
+    cartItems: selectCartItems(state),
   };
 };
 
---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,28 @@
+import { createSelector } from "reselect";
+
+//provider -> connect -> state(store)
+
+/// cart , state { user : userReducer , cart : cartReducer}
+
+// cartReducer -> cart Combine
+
+//humara cart hai
+//constant h , state.cart object h
+const selectCart = (state) => state.cart;
+// selectors ->
+// humare cart ke andar ke items
+export const selectCartItems = createSelector(
+  [selectCart],
+  (cart) => cart.cartItems
+);
+
+//  const variable = createSelector([array of selectors ] , ()=> return  would be stored in the variable)
+export const selectCartItemsCount = createSelector(
+  [selectCartItems], // selector -> selectCartItems
+  (cartItems) =>
+    cartItems.reduce(
+      (accumalatedQuantity, CartItem) =>
+        accumalatedQuantity + CartItem.quantity,
+      0
+    )
+);
